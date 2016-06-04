f6c698cc5f7b973ef11cd40bd43403de7c8ea995
adriano barbosa <b.adrianobarbosa@gmail.com>
2016-06-04 10:10:44 -0300

    alteração

--- www/index.php
+++ www/index.php
@@ -1,8 +1,11 @@
 <?php
 
-    require '../class/sgbd.php';
-    
-    $servidor = new sgbd('mysql');
-    
-    echo $servidor->tipo;
-       
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', '../logs/error_log.txt');
+error_reporting(E_ALL);
+
+require '../class/sgbd.php';
+$servidor = new sgbd('mysql');
+
+echo $servidor->tipo;
